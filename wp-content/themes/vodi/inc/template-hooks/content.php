81ff7c2b5cc220d078eca5efde531a6c0b9a14d3
maxvictor <maxvictorhc@gmail.com>
2020-06-24 21:37:47 -0300

    remove

--- wp-content/themes/vodi/inc/template-hooks/content.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Template Hooks related to site-content
- */
-add_action( 'vodi_content_top', 'vodi_breadcrumb', 10 );
--- wp-content/themes/vodi/inc/template-hooks/widgets.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Template hooks for widgets
- */
-
-add_filter( 'widget_categories_args', 'vodi_modify_widget_categories_args', 10, 2 );
